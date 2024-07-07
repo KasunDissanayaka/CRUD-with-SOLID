044b568494b94476a122ef8999d8dac7a7ec766e
Kasun Dissanayaka <kasun.d@evicio.com>
2024-07-07 13:58:42 +0530

    send order details to the third party API

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -3,6 +3,7 @@ namespace App\Repositories;
 
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class OrderRepository implements OrderRepositoryInterface
 {
@@ -10,6 +11,15 @@ class OrderRepository implements OrderRepositoryInterface
     {
         $data['process_id'] = rand(1, 10);
         $order = Order::create($data);
+        $response = Http::post('https://wibip.free.beeceptor.com/order', [
+            'Order_ID' => $order->id,
+            'Customer_Name' => $order->customer_name,
+            'Order_Value' => $order->order_value,
+            'Order_Date' => $order->created_at->format('Y-m-d H:i:s'),
+            'Order_Status' => $order->order_status,
+            'Process_ID' => $order->process_id
+        ]);
+    
         return $order;
     }
 }
